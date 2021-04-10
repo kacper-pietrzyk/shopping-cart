1873cfe04f33a2dfaca3b6dc7ceb23b5b53ee670
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-10 12:49:48 +0200

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React from 'react';
+import styles from './App.module.scss';
+
+const App = () => {
+
+  return (
+    <div className={styles.wrapper}>
+      Test
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    </div>
+  );
+}
+
+export default App;
