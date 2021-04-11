10d057b5babc028f79f3243ea15c7a66661b96ac
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-11 11:02:02 +0200

    classNames update

--- src/components/ShoppingCart/ProductsList/ProductsList.jsx
+++ src/components/ShoppingCart/ProductsList/ProductsList.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from './ProductsList.module.scss';
 
-import Product from './Product/Product.jsx';
+import Product from './Product/Product';
 
 const ProductsList = () => {
 
   return (
 
     <ul className={styles.productsList}>
-      <li className={`${styles.productsList__product} ${styles.productsList__header}`}>
-        <p className={styles.product__name}>Product Name</p>
-        <p className={styles.product__price}>Unit Price</p>
-        <p className={styles.product__qty}>Qty</p>
+      <li className={styles.productsList__header}>
+        <p className={styles.productsList__name}>Product Name</p>
+        <p className={styles.productsList__price}>Unit Price</p>
+        <p className={styles.productsList__qty}>Qty</p>
       </li>
       <Product />
     </ul>
