3ec1d45a2bdf2832ed41631feec6e0479e9a4dc4
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-11 11:02:23 +0200

    create main structure for Summary

--- src/components/ShoppingCart/Summary/Summary.jsx
+++ src/components/ShoppingCart/Summary/Summary.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './Summary.module.scss';
 
+import Shipping from './Shipping/Shipping';
+import CartTotals from './CartTotals/CartTotals';
+
 const Summary = () => {
 
   return (
-    <div></div>
+    <div className={styles.summary}>
+      <button className={styles.summary__button}>Proceed to checkout</button>
+      <Shipping />
+      <CartTotals />
+    </div>
   );
 }
 
