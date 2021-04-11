8c3cf26ed60fa52d270874354ea970fb2c5381c2
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-11 18:33:36 +0200

    update subtotal and grandTotal values

--- src/components/ShoppingCart/Summary/CartTotals/CartTotals.jsx
+++ src/components/ShoppingCart/Summary/CartTotals/CartTotals.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import styles from './CartTotals.module.scss';
 
-const CartTotals = () => {
+const CartTotals = ({ subtotal, grandTotal }) => {
 
   return (
     <div className={styles.cartTotals}>
       <h2 className={styles.cartTotals__header}>Cart totals</h2>
       <div className={styles.cartTotals__subtotal}>
         <h3 className={styles.cartTotals__subtotalText}>Subtotal</h3>
-        <p>$23.80</p>
+        <p>${subtotal}</p>
       </div>
       <div className={styles.cartTotals__grandTotal}>
         <h3 className={styles.cartTotals__grandTotalText}>Grand Total</h3>
-        <p>$23.80</p>
+        <p>${grandTotal}</p>
       </div>
       <button className={styles.cartTotals__button}>Proceed to checkout</button>
     </div>
