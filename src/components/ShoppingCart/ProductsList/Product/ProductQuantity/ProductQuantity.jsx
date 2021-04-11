4d7a2467542291b93edf17ec0092283841dd0cd6
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-11 16:08:21 +0200

    add function to handle "+" and "-" buttons

--- src/components/ShoppingCart/ProductsList/Product/ProductQuantity/ProductQuantity.jsx
+++ src/components/ShoppingCart/ProductsList/Product/ProductQuantity/ProductQuantity.jsx
@@ -3,17 +3,44 @@ import styles from './ProductQuantity.module.scss';
 
 import editImage from '../../../../../assets/images/edit-img.png';
 
-const ProductQuantity = () => {
+const ProductQuantity = ({ id, quantity, handleButtonQuantity }) => {
+
+  // const handleInputChange = e => {
+  //   if (e.target.value >= 0) {
+  //     setQty(e.target);
+  //   } else {
+  //     setQty(0)
+  //   }
+  // }
+
+  const handleButtonClick = e => {
+    handleButtonQuantity(e);
+  }
 
   return (
     <div className={styles.quantity}>
-      <button className={styles.quantity__button}>-</button>
+      <button
+        className={styles.quantity__button}
+        id={id}
+        name="remove"
+        onClick={handleButtonClick}
+      >
+        -
+        </button>
       <input
         className={styles.quantity__input}
         type="number"
-      // value="1" 
+        value={quantity}
+      // onChange={handleInputChange}
       />
-      <button className={styles.quantity__button}>+</button>
+      <button
+        className={styles.quantity__button}
+        id={id}
+        name="add"
+        onClick={handleButtonClick}
+      >
+        +
+        </button>
       <button className={styles.quantity__edit}>
         <img
           className={styles.quantity__editImg}
