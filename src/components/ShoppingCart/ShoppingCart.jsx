f4b977663254473382cc12e62c9cc3e4c78dd1a5
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-11 16:07:00 +0200

    add function to handle buttons "+" and "-"

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ShoppingCart.module.scss';
 
 import ProductsList from './ProductsList/ProductsList';
 import Summary from './Summary/Summary';
 
 import productsData from '../../assets/data/products.json';
+import productsQuantity from '../../assets/data/cart_products.json';
 
 const ShoppingCart = () => {
 
   const productsList = productsData;
+  const initialQuantity = productsQuantity;
+  const [productsQty, setProductsQty] = useState(initialQuantity);
+
+  const handleButtonQuantity = e => {
+    const index = e.target.id - 1;
+    const action = e.target.name;
+    const newProducstQty = [...productsQty];
+
+    if (action === "remove" && newProducstQty[index].quantity > 0) {
+      newProducstQty[index].quantity--;
+    } else if (action === "add") {
+      newProducstQty[index].quantity++;
+    }
+
+    setProductsQty(newProducstQty);
+  }
 
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.header}>Shopping Cart</h1>
       <main className={styles.productsWrapper}>
-        <ProductsList productsList={productsList} />
+        <ProductsList
+          productsList={productsList}
+          productsQty={productsQty}
+          handleButtonQuantity={handleButtonQuantity}
+        />
         <button className={styles.updateCart}>Update Shopping Cart</button>
       </main>
       <Summary />
