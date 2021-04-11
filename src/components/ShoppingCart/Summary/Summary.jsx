987ca45b40a8b5d8c8f2c581e7b8511e6605db8a
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-11 18:33:11 +0200

    calculate shippingValue and grandTotal

--- src/components/ShoppingCart/Summary/Summary.jsx
+++ src/components/ShoppingCart/Summary/Summary.jsx
@@ -4,13 +4,19 @@ import styles from './Summary.module.scss';
 import Shipping from './Shipping/Shipping';
 import CartTotals from './CartTotals/CartTotals';
 
-const Summary = () => {
+const Summary = ({ subtotal }) => {
+
+  const shippingValue = subtotal > 100 ? 0 : 23.80;
+  const grandTotal = subtotal + shippingValue;
 
   return (
     <div className={styles.summary}>
       <button className={styles.summary__button}>Proceed to checkout</button>
-      <Shipping />
-      <CartTotals />
+      <Shipping shippingValue={shippingValue.toFixed(2)} />
+      <CartTotals
+        subtotal={subtotal.toFixed(2)}
+        grandTotal={grandTotal.toFixed(2)}
+      />
     </div>
   );
 }
