f06c22a74c38b2a155f63d08b39775892445f512
Kacper Pietrzyk <kacper1609@gmail.com>
2021-04-11 12:37:58 +0200

    get data from json file

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import styles from './ShoppingCart.module.scss';
 
-import ProductsList from './ProductsList/ProductsList.jsx';
-import Summary from './Summary/Summary.jsx';
+import ProductsList from './ProductsList/ProductsList';
+import Summary from './Summary/Summary';
+
+import productsData from '../../assets/data/products.json';
 
 const ShoppingCart = () => {
+
+  const productsList = productsData;
 
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.header}>Shopping Cart</h1>
       <main className={styles.productsWrapper}>
-        <ProductsList />
+        <ProductsList productsList={productsList} />
         <button className={styles.updateCart}>Update Shopping Cart</button>
       </main>
       <Summary />
